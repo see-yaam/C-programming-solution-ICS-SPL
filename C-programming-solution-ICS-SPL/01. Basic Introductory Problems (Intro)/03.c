3c9786d30385c35bae0f91c0f9db1325ae4e4d31
Seyam Bhuyan <bhuyanseyam4@gmail.com>
2025-03-27 04:22:34 +0600

    03.c

--- C-programming-solution-ICS-SPL/01. Basic Introductory Problems (Intro)/03.c
+++ C-programming-solution-ICS-SPL/01. Basic Introductory Problems (Intro)/03.c
@@ -1,14 +1,11 @@
-
 /*
 Program that will print the following segment:
-
    +--------------+------------------------------------------------+
    | Sample input | Sample output                                  |
    +--------------+------------------------------------------------+
    |              | The question is - "How to write a              |
    |              | \comment/ in C programming language?"          |
    +--------------+------------------------------------------------+
-
 */
 #include<stdio.h>
 int main()
